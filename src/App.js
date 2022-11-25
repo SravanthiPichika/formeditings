b57a128bcc31db88e56e994fd612dffa787418aa
sravanthipichika <106005445+SravanthiPichika@users.noreply.github.com>
2022-11-25 10:49:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform"
-import About from "./About";
+// import About from "./About";
+import { useState } from "react";
 
 function App() {
+  const [mode,setmode]=useState('light');
+  const[textMode,settextMode]=useState("Dark")
+ const togglemode=()=>{
+  if(mode==='dark'){
+    setmode('light');
+    document.body.style.backgroundColor='white';
+    settextMode("Dark")
+  }
+  else{
+    setmode('dark')
+    document.body.style.backgroundColor='grey'
+    settextMode("Light")
+
+  }
+ }
   return (
     <>
-      <Navbar title="SRAVANTHI" about="ABOUT US" />
+      <Navbar title="SRAVANTHI" about="ABOUT US" mode={mode} togglemode={togglemode} textMode={textMode}
+      />
       <div className="container my-3">
-        <Textform heading="Enter your text"/>
-        <About />
+        <Textform heading="Enter your text" mode={mode}  />
+ {/* <About />*/}
       </div>
     </>
   );
